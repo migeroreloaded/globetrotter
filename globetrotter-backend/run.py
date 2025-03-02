c0d505ea9034633a7020b4b021e7b1e08f53bddf
Migero <mikeroche138@gmail.com>
2025-03-02 14:09:02 +0300

    solve CORS issue

--- globetrotter-backend/run.py
+++ globetrotter-backend/run.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": ["Content-Type"]}}))
 
 @app.route("/")
 def home():
