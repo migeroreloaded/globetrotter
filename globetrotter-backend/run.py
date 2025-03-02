09d77d74ec4c17ee69810725d618a16f41713657
Migero <mikeroche138@gmail.com>
2025-03-02 13:22:58 +0300

    Fix CORS preflight request issue

--- globetrotter-backend/run.py
+++ globetrotter-backend/run.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app, supports_credentials=True)
 
 @app.route("/")
 def home():
